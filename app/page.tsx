96c7c4f7018e6163d6a922ae74d6df100fefac32
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-08 19:24:05 +0200

    Add utility class for centering elements and simplify Home component layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-amber-100">
+    <div className="center bg-amber-100">
       <h1 className="text-3xl font-bold text-brand">
         StoreIt - The only storage solution you need.
       </h1>
