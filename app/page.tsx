5ba8d12c1d74a1fc127cfcff9ac730b85214ad5c
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-08 21:12:26 +0200

    Refactor Home component layout to use flex-center utility for improved alignment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="center bg-amber-100">
+    <div className="flex-center h-screen">
       <h1 className="text-3xl font-bold text-brand">
         StoreIt - The only storage solution you need.
       </h1>
