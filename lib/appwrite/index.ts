d48f1045cb310f4da624140b6c7530385ab34493
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-09 00:20:07 +0200

    Implement Appwrite client creation functions for session and admin access

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,46 @@
+import { cookies } from "next/headers";
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { API_KEY, ENDPOINT, PROJECT_ID } from "./config";
+
+export const createSessionClient = async () => {
+  const client = new Client().setEndpoint(ENDPOINT!).setProject(PROJECT_ID!);
+
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) {
+    throw new Error("No session found");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(ENDPOINT!)
+    .setProject(PROJECT_ID!)
+    .setKey(API_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
