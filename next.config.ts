167a18aeaf186104ea8bb2625507ecac6877a204
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-09 18:45:28 +0200

    Add image remote patterns for avatar and cloud app hosting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
