936dbd85c5df832d1797190a0c5e21970a1e2d3e
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-09 18:04:21 +0200

    Add Header, FileUploader, and Search components with initial structure

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+
 export default function Header() {
-  return <div>header</div>;
+  return (
+    <header className="header">
+      <Search />
+
+      <div className="header-wrapper">
+        <FileUploader />
+
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="Sign Out"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
 }
