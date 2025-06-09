83a8b2e56cfd346fe113354ec76d36d25175c801
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-09 12:18:23 +0200

    Add OTP input fields to OTPModal for user verification

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -9,6 +9,12 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 type Props = {
   email: string;
@@ -30,6 +36,21 @@ export default function OTPModal({ email, accountId }: Props) {
             account and remove your data from our servers.
           </AlertDialogDescription>
         </AlertDialogHeader>
+
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+        </InputOTP>
+
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction>Continue</AlertDialogAction>
