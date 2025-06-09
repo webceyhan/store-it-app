aa9f70c5ab50b297f36281b285d58c9172aaeb93
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-09 18:35:18 +0200

    Implement Sidebar component with navigation and logo images

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,58 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { navItems } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Sidebar() {
-  return <div>sidebar</div>;
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo-full-brand.svg"
+          alt="logo"
+          width={160}
+          height={50}
+          className="max-lg:hidden h-auto"
+        />
+
+        <Image
+          src="/assets/icons/logo-brand.svg"
+          alt="logo"
+          width={52}
+          height={52}
+          className="lg:hidden"
+        />
+      </Link>
+
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map(({ url, name, icon }) => {
+            const isActive = pathname === url;
+
+            return (
+              <Link href={url} key={name} className="lg:w-full">
+                <li
+                  className={cn("sidebar-nav-item", isActive && "shad-active")}>
+                  <Image
+                    src={icon}
+                    alt={name}
+                    width={24}
+                    height={24}
+                    className={cn("nav-icon", isActive && "nav-icon-active")}
+                  />
+
+                  <p className="max-lg:hidden">{name}</p>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </nav>
+    </aside>
+  );
 }
