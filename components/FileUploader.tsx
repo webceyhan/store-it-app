9ef9346626c6a5455a6ce5f03193e9640842242d
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-09 21:29:59 +0200

    Implement FileUploader component with drag-and-drop file upload functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,3 +1,71 @@
-export default function FileUploader(){
-    return <div>file uploader</div>
+"use client";
+
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, getFileType } from "@/lib/utils";
+import Image from "next/image";
+
+type File = {
+  name: string;
+};
+
+type Props = {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+};
+
+export default function FileUploader({ ownerId, accountId, className }: Props) {
+  //
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="Upload"
+          width={24}
+          height={24}
+        />
+        <p>Upload</p>
+      </Button>
+
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item">
+                <div className="flex items-center gap-3">
+                  {/* implement later... */}
+                  Thumbnail {file.name}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 }
