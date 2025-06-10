cd50287c6c18abd9075291993dec0325d85d1936
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-10 19:14:01 +0200

    Add additional remote pattern for image loading from fra.cloud.appwrite.io

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cloud.appwrite.io",
       },
+      {
+        protocol: "https",
+        hostname: "fra.cloud.appwrite.io",
+      },
     ],
   },
 };
