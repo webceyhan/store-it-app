5314ae101fb9fc2d9ddd938e6b95a9c9ebd6f770
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-10 19:14:12 +0200

    Add FormattedDateTime component for date formatting and display

---
+++ components/FormattedDateTime.tsx
@@ -0,0 +1,14 @@
+import { cn, formatDateTime } from "@/lib/utils";
+
+type Props = {
+  date: string;
+  className?: string;
+};
+
+export default function FormattedDateTime({ date, className }: Props) {
+  return (
+    <time className={cn("body-1 text-light-200", className)}>
+      {formatDateTime(date)}
+    </time>
+  );
+}
