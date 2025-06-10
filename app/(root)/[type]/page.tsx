f4032d4abb6e2441efb634b6fe18eed254a888fc
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-10 17:57:43 +0200

    Add Sort component and integrate it into Page for sorting functionality

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,3 +1,5 @@
+import Sort from "@/components/Sort";
+
 export default async function Page({ params }: { params: { type: string } }) {
   const { type } = await params;
 
@@ -5,7 +7,20 @@ export default async function Page({ params }: { params: { type: string } }) {
     <div className="page-container">
       <section className="w-full">
         <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total: <span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 max-sm:hidden text-light-200">Sort by:</p>
+            <Sort />
+          </div>
+        </div>
       </section>
+
+      {/* render the files here */}
     </div>
   );
 }
