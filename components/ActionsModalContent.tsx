3603aab9f1f8397d1e0493d36e184bf5f3341597
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-11 15:22:16 +0200

    Refactor ActionsModalContent component to improve type definitions and add ShareInput functionality for sharing files with users

--- components/ActionsModalContent.tsx
+++ components/ActionsModalContent.tsx
@@ -1,13 +1,21 @@
 import { Models } from "node-appwrite";
-import Thumbnail from "./Thumbnail";
+import Image from "next/image";
+
 import { constructFileUrl, convertFileSize, formatDateTime } from "@/lib/utils";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
 
 export default function ActionsModalContent({}) {
   return <div>demo</div>;
 }
 
-const ImageThumbnail = ({ file }: { file: Models.Document }) => {
+type ImageThumbnailProps = {
+  file: Models.Document;
+};
+
+const ImageThumbnail = ({ file }: ImageThumbnailProps) => {
   return (
     <div className="file-details-thumbnail">
       <Thumbnail
@@ -23,7 +31,12 @@ const ImageThumbnail = ({ file }: { file: Models.Document }) => {
   );
 };
 
-const DetailRow = ({ label, value }: { label: string; value: string }) => {
+type DetailRowProps = {
+  label: string;
+  value: string;
+};
+
+const DetailRow = ({ label, value }: DetailRowProps) => {
   return (
     <div className="flex">
       <p className="file-details-label">{label}</p>
@@ -32,7 +45,11 @@ const DetailRow = ({ label, value }: { label: string; value: string }) => {
   );
 };
 
-export const FileDetails = ({ file }: { file: Models.Document }) => {
+type FileDetailsProps = {
+  file: Models.Document;
+};
+
+export const FileDetails = ({ file }: FileDetailsProps) => {
   return (
     <>
       <ImageThumbnail file={file} />
@@ -42,6 +59,64 @@ export const FileDetails = ({ file }: { file: Models.Document }) => {
         <DetailRow label="Size:" value={convertFileSize(file.size)} />
         <DetailRow label="Owner:" value={file.owner.fullName} />
         <DetailRow label="Last edit:" value={formatDateTime(file.$updatedAt)} />
+      </div>
+    </>
+  );
+};
+
+type ShareInputProps = {
+  file: Models.Document;
+  onChange: React.Dispatch<React.SetStateAction<string[]>>;
+  onRemove: (email: string) => void;
+};
+
+export const ShareInput = ({ file, onChange, onRemove }: ShareInputProps) => {
+  return (
+    <>
+      <ImageThumbnail file={file} />
+
+      <div className="share-wrapper">
+        <p className="subtitle-2 pl-1 text-light-100">
+          Share file with other users
+        </p>
+
+        <Input
+          type="email"
+          placeholder="Enter email"
+          className="share-input-field"
+          onChange={(e) => onChange(e.target.value.trim().split(","))}
+        />
+
+        <div className="pt-4">
+          <div className="flex justify-between">
+            <p className="subtitle-2 text-light-100"> Shared with</p>
+            <p className="subtitle-2 text-light-200">
+              {file.users.length} user{file.users.length > 1 ? "s" : ""}
+            </p>
+          </div>
+
+          <ul className="pt-2">
+            {file.users.map((email: string) => (
+              <li
+                key={email}
+                className="flex items-center justify-between gap-2">
+                <p className="subtitle-2">{email}</p>
+
+                <Button
+                  className="share-remove-user"
+                  onClick={() => onRemove(email)}>
+                  <Image
+                    className="remove-icon"
+                    src="/assets/icons/remove.svg"
+                    alt="Remove"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
