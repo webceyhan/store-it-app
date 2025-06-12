809121de8d7d1a3c50c5b2c254ce6f520804d2c5
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-12 12:20:12 +0200

    Implement Sort component with dynamic sorting functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+
 export default function Sort() {
-  return <div>sort</div>;
+  //
+  const path = usePathname();
+  const router = useRouter();
+
+  const handleSortChange = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handleSortChange} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+
+      <SelectContent className="sort-select-content">
+        {sortTypes.map(({ label, value }) => (
+          <SelectItem key={value} value={value} className="shad-select-item">
+            {label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 }
