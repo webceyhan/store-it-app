e11d1edfd6f1a72e1fdd154f63224a8c80f20d52
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-13 23:00:34 +0200

    Refactor StatsChart component to integrate RadialBarChart for visualizing storage usage and update styles in globals.css

--- components/StatsChart.tsx
+++ components/StatsChart.tsx
@@ -1,13 +1,36 @@
-import { convertFileSize, getUsageSummary } from "@/lib/utils";
+"use client";
+
+import {
+  Label,
+  PolarGrid,
+  PolarRadiusAxis,
+  RadialBar,
+  RadialBarChart,
+} from "recharts";
+
+import {
+  calculatePercentage,
+  convertFileSize,
+  getUsageSummary,
+} from "@/lib/utils";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "./ui/card";
+} from "@/components/ui/card";
 
-const MAX_SPACE = 2 * 1024 * 1024 * 1024; // 2 GB in bytes
+const CHART_CONFIG = {
+  size: {
+    label: "Size",
+  },
+  used: {
+    label: "Used",
+    color: "white",
+  },
+} satisfies ChartConfig;
 
 type Props = {
   summaryList: ReturnType<typeof getUsageSummary>;
@@ -15,14 +38,69 @@ type Props = {
 
 export default function StatsChart({ summaryList }: Props) {
   //
-  const usedSize = summaryList.reduce((acc, { size }) => acc + size, 0);
-  const totalCount = summaryList.reduce((acc, { count }) => acc + count, 0);
-  const percentageUsed = ((usedSize / MAX_SPACE) * 100).toFixed(2);
-  const remainingSpace = MAX_SPACE - usedSize;
+  const usedSize =
+    summaryList.reduce((acc, { size }) => acc + size, 0);
+  const usedPercentage = calculatePercentage(usedSize);
+
+  const chartData = [
+    {
+      storage: "used",
+      used: usedSize,
+      fill: "white",
+    },
+  ];
 
   return (
     <Card className="chart">
-      <CardContent className="flex-1 p-0"></CardContent>
+      <CardContent className="flex-1 p-0">
+        <ChartContainer config={CHART_CONFIG} className="chart-container">
+          <RadialBarChart
+            data={chartData}
+            startAngle={90}
+            endAngle={90 - usedPercentage * 3.6}
+            innerRadius={80}
+            outerRadius={140}>
+            <PolarGrid
+              gridType="circle"
+              radialLines={false}
+              stroke="none"
+              className="polar-grid"
+              polarRadius={[86, 74]}
+            />
+
+            <RadialBar dataKey="used" background />
+
+            <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle">
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="chart-total-percentage">
+                          {usedPercentage}%
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-white/70">
+                          Space used
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </PolarRadiusAxis>
+          </RadialBarChart>
+        </ChartContainer>
+      </CardContent>
 
       <CardHeader className="chart-details">
         <CardTitle className="chart-title">Available Storage</CardTitle>
