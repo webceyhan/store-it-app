19a756605ce5c55eb6ca7736a46737cd1b861f6d
Evren Ceyhan <evren.ceyhan@gmail.com>
2025-06-14 20:22:26 +0200

    Replace error handling in getCurrentUser with console log for debugging purposes

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -103,7 +103,7 @@ export const getCurrentUser = async () => {
 
     return parseStringify(user.documents[0]);
   } catch (error) {
-    handleError(error, "Failed to get current user");
+    console.log("Error fetching current user:", error);
   }
   return null;
 };
